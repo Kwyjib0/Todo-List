42de682ec3ba926d11b75fb7509cafa317d36c5a
Chris Linton <chris.linton@mail.com>
2022-05-09 10:51:49 -0400

    new model fields added

--- todo_list/todo/models.py
+++ todo_list/todo/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     task_id = models.AutoField(primary_key=True)
     task = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task;
+
+    class Meta:
+        # put completed items last
+        ordering = ['is_done']
