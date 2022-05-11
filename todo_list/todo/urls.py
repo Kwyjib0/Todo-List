8f4eb441a84b5e64b69d3da211a70f917c9a84a0
Chris Linton <chris.linton@mail.com>
2022-05-11 11:56:01 -0400

    new user registration

--- todo_list/todo/urls.py
+++ todo_list/todo/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    # no custom view needed,using view directly, 
+    # just need to pass in next page attribute on where to redirect after loggin out
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', views.RegisterPage.as_view(), name='register'),
+
     path('', views.TaskList.as_view(), name='list'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
     path('task-create/', views.TaskCreate.as_view(), name='task-create'),
